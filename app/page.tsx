a64fd6eae4bde3ed8eaf92123bb5fadab923d7b9
Kartik Gothwal <kartikgothwal28@gmail.com>
2024-04-03 19:12:52 +0530

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import React from "react";
-
-const App = () => {
-  return <div>Index page</div>;
+ 
+const Page = () => {
+  return (
+      <div>Index page</div>
+  );
 };
 
-export default App;
+export default Page;
